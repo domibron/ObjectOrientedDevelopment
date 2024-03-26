7e9e9e3993d8254bf13f600b89f4749ac503d08b
DM <dommcneill@outlook.com>
2024-03-26 23:44:18 +0000

    Started some work

---
+++ RobGameSolution/RobGame/RobGame/Core/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobGame.Core
+{
+    public class GameObject
+    {
+        public Vector2 Position;
+        public Vector2 Direction;
+
+        public GameObject()
+        {
+            Position = Vector2.Zero;
+            Direction = Vector2.Zero;
+        }
+
+        public GameObject(Vector2 position)
+        {
+            Position = position;
+            Direction = Vector2.Zero;
+        }
+
+        public GameObject(Vector2 position, Vector2 direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+    }
+}
