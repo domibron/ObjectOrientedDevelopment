05c5fd509450edb5dc321a29ff0e9e4987f05244
DM <dommcneill@outlook.com>
2024-04-28 15:50:47 +0100

    Finished the systems classes

--- RobGameSolution/RobGame/System/Input.cs
+++ RobGameSolution/RobGame/System/Input.cs
@@ -9,6 +9,28 @@ namespace System
 {
     public static class Input
     {
-        
+        public static string? GetInput(int min, int max)
+        {
+            string? input = Console.ReadLine();
+
+            if (input == null)
+            {
+                return null;
+            }
+
+            if (!Util.ValidateInput(input, min, max))
+            {
+                return null;
+            }
+
+            return input;
+        }
+
+        public static ConsoleKey GetControlInput()
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            return keyInfo.Key;
+        }
     }
 }
