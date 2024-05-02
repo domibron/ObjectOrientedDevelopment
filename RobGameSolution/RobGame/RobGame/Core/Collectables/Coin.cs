ea2eb2b3d18fac816881743708ebd1bd3162315d
DM <dommcneill@outlook.com>
2024-05-02 03:34:45 +0100

    Continue implementation

---
+++ RobGameSolution/RobGame/RobGame/Core/Collectables/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static RobGame.Core.Vector2Util;
+
+namespace RobGame.Core.Collectables
+{
+    public class Coin : Collectable
+    {
+
+        public Coin(int id, Vector2Int position, Vector2 rotation) : base(id, position, rotation) { 
+        
+        }
+
+        public Coin(int id) : base(id)
+        {
+
+        }
+
+        public override void Collect()
+        {
+
+        }
+    }
+}
---
+++ RobGameSolution/RobGame/RobGame/Core/Collectables/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static RobGame.Core.Vector2Util;
+
+namespace RobGame.Core.Collectables
+{
+    public abstract class Collectable : GameObject
+    {
+        public virtual int ID { get; }
+
+        public virtual void Collect() { }
+
+        public Collectable(int id, Vector2Int position, Vector2 rotation) : base(position, rotation)
+        {
+            ID = id;
+        }
+
+        public Collectable(int id) : base()
+        {
+            ID = id;
+        }
+    }
+}
