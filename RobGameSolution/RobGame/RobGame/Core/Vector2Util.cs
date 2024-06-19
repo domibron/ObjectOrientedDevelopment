b7cb69a258c2b18f6ad952712a391d3bf710b049
DM <dommcneill@outlook.com>
2024-06-20 00:53:00 +0100

    manage to improve vector2int and added check colision on game mangagere

--- RobGameSolution/RobGame/RobGame/Core/Vector2Util.cs
+++ RobGameSolution/RobGame/RobGame/Core/Vector2Util.cs
@@ -6,25 +6,34 @@ using System.Threading.Tasks;
 
 namespace RobGame.Core
 {
-    public class Vector2Util
+    
+    public struct Vector2Int
     {
-        public struct Vector2Int
+        public int X;
+        public int Y;
+
+        public Vector2Int(int x, int y)
         {
-            public int X;
-            public int Y;
+            this.X = x;
+            this.Y = y;
+        }
 
-            public Vector2Int(int x, int y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
+        public static Vector2Int Zero = new Vector2Int(0, 0);
+        public static Vector2Int Up = new Vector2Int(0, -1);
+        public static Vector2Int Down = new Vector2Int(0, 1);
+        public static Vector2Int Left = new Vector2Int(1, 0);
+        public static Vector2Int Right = new Vector2Int(-1, 0);
+        public static Vector2Int One = new Vector2Int(1, 1);
 
-            public static Vector2Int Zero = new Vector2Int(0, 0);
-            public static Vector2Int Up = new Vector2Int(0, -1);
-            public static Vector2Int Down = new Vector2Int(0, 1);
-            public static Vector2Int Left = new Vector2Int(1, 0);
-            public static Vector2Int Right = new Vector2Int(-1, 0);
-            public static Vector2Int One = new Vector2Int(1, 1);
+        public static Vector2Int operator +(Vector2Int a, Vector2Int b)
+        {
+            return new Vector2Int(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector2Int operator -(Vector2Int a, Vector2Int b)
+        {
+            return new Vector2Int(a.X - b.X, a.Y - b.Y);
         }
     }
+    
 }
